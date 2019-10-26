687e8f7aaec6f15776165ef5c3f178a36e342ea3
Mattias Jonsson <skmj.mattias@gmail.com>
2019-10-26 22:42:04 +0200

    update with text overlay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 
 var green = getComputedStyle(document.documentElement).getPropertyValue('--green');
 
+/// Set image aspect ration.
+function setAspectRatio(w, h) {
+    widthRatio = w;
+    heightRatio = h;
+    var width = $("#uploaded-image-div").width();
+    var height = width / w * h;
+    $("#uploaded-image-div").height(height);
+
+    setTextPosition();
+}
+
+/// Set the uploaded image.
 function setImage(event) {
     // Set the image.
     var getImagePath = URL.createObjectURL(event.target.files[0]);
@@ -12,6 +24,32 @@ function setImage(event) {
 
     // Set the filter.
     $('#green-filter').css('background-color', green);
+}
+
+/// Set filter opacity.
+function setFilter(opacity) {
+    $("#uploaded-image-div").css("opacity", opacity);
+}
+
+// Update the overlay text position.
+function setTextPosition() {
+    var image = $('#image-capture');
+    var imagePos = image.position();
+    var text = $('#overlay-text');
+
+    var left = imagePos.left + image.width()/2 - text.outerWidth()/2 - 15;
+    var top = imagePos.top + image.height()/2 - text.outerHeight()/2;
+
+    text.css('left', left + 'px');
+    text.css('top', top + 'px');
+}
+
+/// Set the overlay text.
+function setText() {
+    var input = $('#overlay-text-input').val();
+    $('#overlay-text').html(input);
+
+    setTextPosition();
 }
 
 function hh(element) {
